2a5a97bc4498b9f9c525af69b9abb66e4afffaeb
Moe Sanjari <sanjari@gmail.com>
2023-12-02 22:34:34 -0600

    first commit

---
+++ openai_config.py
@@ -0,0 +1,7 @@
+from os import environ
+
+MILVUS_HOST = "localhost"
+MILVUS_PORT = "19530"
+OPENAI_API_KEY = "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" # example: "sk-xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"
+
+environ["OPENAI_API_KEY"] = OPENAI_API_KEY
